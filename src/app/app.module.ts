e9817ae243846b522dd5f1f1594b73a0afa809b1
daniel <daniel@taskbase.com>
2021-04-21 14:21:23 +0200

    run ng update @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NotifyModule } from '@tsmean/toast';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {NotifyModule} from '@tsmean/toast';
+import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule} from '@angular/material';
-
-import 'hammerjs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -27,4 +28,5 @@ import 'hammerjs';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
